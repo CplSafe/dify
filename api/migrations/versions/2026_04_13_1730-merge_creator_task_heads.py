d3946a8758d77779d33faf4a60ff9016b560c6f6
CplSafe <1441333484@qq.com>
2026-04-13 17:30:55 +0800

    chore: merge creator task migration heads

---
+++ api/migrations/versions/2026_04_13_1730-merge_creator_task_heads.py
@@ -0,0 +1,21 @@
+"""merge creator task heads
+
+Revision ID: d4e5f6a7b8c9
+Revises: b2c3d4e5f6g7, a3f7c2d1e8b9
+Create Date: 2026-04-13 17:30:00.000000
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'd4e5f6a7b8c9'
+down_revision = ('b2c3d4e5f6g7', 'a3f7c2d1e8b9')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
